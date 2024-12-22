049b44d74409502835629f69b61db90f98dfed37
Abdulrahmanmoin <iam.armoin@gmail.com>
2024-12-22 20:05:23 +0500

    first commit

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const page = async (props: any) => {
+
+
+    const url = await fetch(`https://jsonplaceholder.typicode.com/posts/${props.params.id}`)
+    const res = await url.json()
+
+
+    return (
+        <div className='bg-blue-600 m-10'>
+            <p className='p-4'>
+                User ID: {res.userId}
+            </p>
+            <br></br>
+            <p className='p-4'>
+                TITLE: {res.title}
+            </p>
+            <br></br>
+            <p className='p-4'>
+                DESCRIPTION: {res.body}
+            </p>
+        </div>
+    )
+}
+
+export default page
